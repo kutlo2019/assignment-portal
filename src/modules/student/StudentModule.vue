81a6de9fe1f997488c5fcc3bd4da560642a61175
Kutlo <57438497+kutlo2019@users.noreply.github.com>
2024-06-06 18:00:51 +0200

    render side bar conditionally

--- src/modules/student/StudentModule.vue
+++ src/modules/student/StudentModule.vue
@@ -2,7 +2,7 @@
   <HeaderComponent />
   <div class="container-fluid">
     <div class="row">
-      <SideBarComponent />
+      <SideBarComponent v-if="showSideBar()" />
       <router-view />
     </div>
   </div>
@@ -10,5 +10,13 @@
 
 <script setup lang="ts">
 import HeaderComponent from "./components/HeaderComponent.vue"
-import SideBarComponent from "./components/SideBarComponent.vue";
+import SideBarComponent from "./components/SideBarComponent.vue"
+import { useRoute } from 'vue-router';
+
+const route = useRoute();
+
+const showSideBar = () => {
+  let inlcudesAssignment = route.path.includes("assignment", 21)
+  return !inlcudesAssignment
+};
 </script>
